3fedcd536a7b04695943fc55c624de27a823046d
Marco Sumali <marco.sumali@2359media.com>
2021-03-30 01:38:03 +0700

    chore: revise styling and remove unused

--- client/svelte-quiz-dynamic/src/App.svelte
+++ client/svelte-quiz-dynamic/src/App.svelte
@@ -1,5 +1,4 @@
 <script>
-  // import { Router, Link, Route } from "svelte-routing";
   import "wired-elements";
 
   import Home from "./views/Home/index.home.svelte";
@@ -24,35 +23,16 @@
     },
   };
   interactiveCanvas.ready(callbacks);
-
-  // export let url = "";
 </script>
 
-<!-- <Router url={url}>
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/quiz">Quiz</Link>
-    <Link to="/end">End</Link>
-  </nav>
-  <div>
-    <Route path="/" component={Home} />
-    <Route path="/quiz" component={Quiz} />
-    <Route path="/end" component={End} />
-    <Route path="*" component={NotFound} />
-  </div>
-</Router> -->
-
 <div>
-  {$navRoutes}
-  {$index}
-  {JSON.stringify($quiz)}
   {#if $navRoutes === HOME}
-    <Home />
-  {:else if $navRoutes === QUIZ}
     <Quiz />
+  {:else if $navRoutes === QUIZ}
+    <Home />
   {:else if $navRoutes === END}
     <End />
-    {:else}
+  {:else}
     <NotFound />
   {/if}
 </div>
